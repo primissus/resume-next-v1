f052228ae476d057fbae22cfabaa9093e27faaa9
Manuel Ruiz <manuel.rivas@resser.com>
2023-08-17 14:05:17 -0600

    Remove trailing slashed

--- src/server/components/LayoutHeader/index.tsx
+++ src/server/components/LayoutHeader/index.tsx
@@ -1,18 +1,21 @@
 import { useTranslator } from '@server/hooks/useTranslator';
 import LocaleLink from '@server/components/LocaleLink';
 
+const NavItem = ({ children, href }: { children: string; href: string; }) => (
+    <li><LocaleLink href={href}><span className="inline-block p-2">{children}</span></LocaleLink></li>
+);
+
+
 export default function LayoutHeader() {
     const t = useTranslator('home');
 
     return (
-        <div className="p-4">
-            <nav>
-                <ul className="flex flex-row gap-x-2">
-                    <li><LocaleLink href="/">{t('home')}</LocaleLink></li>
-                    <li><LocaleLink href="about-this-page">{t('about')}</LocaleLink></li>
-                </ul>
-            </nav>
-        </div>
+        <nav>
+            <ul className="flex flex-row">
+                <NavItem href="/">{t('home')}</NavItem>
+                <NavItem href="about-this-page">{t('about')}</NavItem>
+            </ul>
+        </nav>
     );
 }
 
--- src/server/components/LocaleLink/index.tsx
+++ src/server/components/LocaleLink/index.tsx
@@ -3,11 +3,19 @@ import { useLocaleRaw } from '@server/hooks/useLocale';
 
 interface LocaleLinkProps extends LinkProps {
   children: React.ReactElement | string;
+  href: string;
+}
+
+function removeTrailingSlash(path: string) {
+    if (path.endsWith('/')) {
+        return path.slice(0, -2);
+    }
+    return path;
 }
 
 export default function LocaleLink({ href: hrefRaw, children, ...linkProps }: LocaleLinkProps) {
   const locale = useLocaleRaw();
-  const href = locale ? `/${locale}/${hrefRaw}` : hrefRaw;
+  const href = removeTrailingSlash(locale ? `/${locale}/${hrefRaw}` : hrefRaw);
 
   return <Link href={href} {...linkProps}>{children}</Link>
 }
