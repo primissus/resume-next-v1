7118983e5a878bf2d2a9c3ff04b256fd2ce5a0e6
Manuel Ruiz <manuel.rivas@resser.com>
2023-08-31 01:28:18 -0600

    Image now fills the screen

--- src/components/GlitchImg/index.tsx
+++ src/components/GlitchImg/index.tsx
@@ -6,12 +6,16 @@ import TintedImg from '@components/TintedImg';
 import styles from './styles.module.scss';
 
 const GlitchImg = ({ src, className }: any) => {
+    //const ratioStyle = `aspect-[${src.width}/${src.height}]`
+    const ratioStyle = `aspect-[${src.height}/${src.width}]`
+
+    //console.log(ratioStyle);
 
     return (
         <div className={clsx(className, styles.imgContainer)}>
-            <TintedImg color="#e73396" className={clsx(styles.filterImg)} src={src} alt="" />
-            <TintedImg color="#1d8ac9" className={clsx(styles.filterImg)} src={src} alt="" />
-            <Image className={clsx(styles.originalImg , 'grayscale')} src={src} alt="" />
+            <TintedImg fill color="#e73396" className={clsx(styles.filterImg, ratioStyle)} src={src} alt="" />
+            <TintedImg fill color="#1d8ac9" className={clsx(styles.filterImg, ratioStyle)} src={src} alt="" />
+            <Image className={clsx(styles.originalImg, 'grayscale', 'max-w-fit', ratioStyle)} src={src} alt="" />
         </div>
     );
 }
