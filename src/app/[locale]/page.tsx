55effd908b1ece649c983b5dcf4fd9e1fb02b04e
Manuel Ruiz <rur.manuel@gmail.com>
2023-09-01 02:28:41 -0600

    Fix image styles (#7)
    * Image now fills the screen
    
    * Better set image
    
    ---------
    
    Co-authored-by: Manuel Ruiz <manuel.rivas@resser.com>

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,25 +1,33 @@
+import clsx from 'clsx';
 import GlitchImg from '@components/GlitchImg';
 import ImgProfile from '@assets/profile.png';
 import { useTranslator } from '@server/hooks/useTranslator';
 import { locales } from '@src/i18n';
 import { provideLocale } from '@server/hooks/useLocale';
 import { LocaleComponentProps } from './types';
+import getRatioStyle from '@lib/getRatioStyle';
+import styles from './styles.module.scss';
 
 export default function Home({ params: { locale } }: LocaleComponentProps) {
     provideLocale(locale);
     const t = useTranslator('home');
+    const ratioStyle = getRatioStyle(ImgProfile);
 
     return (
         <div className="h-full flex flex-col">
-            <div className="h-full flex flex-row items-center">
-                <p className="text-[40px]">{t('helloMessage')}</p>
-                <div className="max-w-[1000px] w-full h-full overflow-hidden">
+            <div className="h-full flex flex-row items-center justify-between overflow-hidden relative">
+                <div className="flex w-[30%] z-10">
+                    <div>
+                        <p className={clsx(styles.greetingText, 'text-[40px] w-[85vw] max-w-[800px]')}>{t('helloMessage')}</p>
+                    </div>
+                </div>
+                <div className="max-w-[1000px] h-full overflow-hidden absolute md:relative bottom-0 left-16 max-h-[650px] md:max-h-max" style={{ aspectRatio: `${ratioStyle}`}}>
                     <GlitchImg src={ImgProfile} className="w-full h-full" />
                 </div>
             </div>
         </div>
-    );
-}
+        );
+    }
 
 export async function generateStaticParams() {
     return locales.map((locale) => ({ locale }));
